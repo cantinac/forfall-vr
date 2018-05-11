4a83069333822a8a8fb554cd910733004fd8608b
Scott Doxey <neogeek0101@gmail.com>
2018-05-11 18:12:19 -0400

    Added bucket with items to throw at tower.

---
+++ Assets/Scripts/BucketController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BucketController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] itemsInBucketPrefabs;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.name.Equals("Hand"))
+        {
+
+            GameObject item = Instantiate(
+                itemsInBucketPrefabs[Random.Range(0, itemsInBucketPrefabs.Length)],
+                other.gameObject.transform.position,
+                other.gameObject.transform.rotation
+            );
+
+            item.GetComponent<Rigidbody>().isKinematic = true;
+
+            item.transform.SetParent(other.gameObject.transform);
+
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+
+    public void ReleaseItem()
+    {
+
+        GameObject item = gameObject.transform.Find("Item").gameObject;
+
+        Rigidbody itemRigidbody = item.GetComponent<Rigidbody>();
+        Rigidbody handRigidbody = gameObject.transform.GetComponent<Rigidbody>();
+
+        itemRigidbody.isKinematic = false;
+
+        itemRigidbody.velocity = handRigidbody.velocity;
+        itemRigidbody.angularVelocity = handRigidbody.angularVelocity;
+
+    }
+
+}
